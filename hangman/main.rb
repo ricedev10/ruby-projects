c1658372c036d75dc95881d42d8ab03b507f7630
Kai Hong <ricedev10@gmail.com>
2025-11-18 17:01:03 -0500

    add gameplay functionality

--- hangman/main.rb
+++ hangman/main.rb
@@ -1,3 +1,4 @@
+require 'io/console'
 require_relative 'lib/dictionary'
 require_relative 'lib/hangman'
 
@@ -7,3 +8,23 @@ end
 
 answer = words.random_word
 game = Hangman.new(answer)
+
+puts 'Welcome to hangman! Enter a letter to start guessing (also may enter words):'
+puts game.status
+loop do
+  input = gets.chomp
+  if input.length == 1
+    game.guess_letter(input)
+  else
+    game.guess_word(input)
+  end
+  status = game.status
+
+  puts status
+  puts "Attempts: #{game.attempts}"
+  puts "Guessed: #{game.incorrect}"
+  if game.won?
+    puts 'You won!'
+    break
+  end
+end
