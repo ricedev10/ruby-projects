d3a09cb49d3e7278cecc6f9641a2f9136363911b
Kai Hong <ricedev10@gmail.com>
2025-11-18 12:55:24 -0500

    encapsulate

--- hangman/lib/hangman.rb
+++ hangman/lib/hangman.rb
@@ -6,7 +6,18 @@ class Hangman
 
   def guess(letter)
     @guesses << letter.downcase
+  end
 
+  def status
+    response = []
+    @word.split('').each do |char|
+      response << (@guesses.include?(char) ? char : '_') << ' '
+    end
+
+    response.join
+  end
+
+  def incorrect_guesses
     response = []
     @word.split('').each do |char|
       response << (@guesses.include?(char) ? char : '_') << ' '
