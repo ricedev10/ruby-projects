6dd7ad22a9e1478e83f0ebaedf7d0eb8c0cd2cea
Kai Hong <ricedev10@gmail.com>
2025-11-18 21:47:29 -0500

    add save class

---
+++ hangman/lib/save.rb
@@ -0,0 +1,37 @@
+require 'json'
+require 'pathname'
+
+class Save
+  attr_reader :saves
+
+  def initialize(dir)
+    Dir.mkdir(dir) unless Dir.exist?(dir)
+    @dir = dir
+    @saves = []
+
+    load_saves
+    p @saves
+  end
+
+  def serialize
+    @saves.each_index do |i|
+      content = JSON.dump(@saves[i])
+      File.write(File.join(@dir, "#{i}.json"), content)
+    end
+  end
+
+  def deserialize(path)
+    obj = JSON.parse(File.read(path))
+    return unless obj.is_a?(Hash) && obj['dir']
+
+    @saves << obj if Pathname.new(obj['dir']).realpath == Pathname.new(@dir).realpath
+  rescue JSON::ParserError => e
+    puts "Could not parse json: #{path}"
+  end
+
+  def load_saves
+    Pathname(@dir).each_child do |child_dir|
+      deserialize(child_dir)
+    end
+  end
+end
